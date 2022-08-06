c7a7e041a6b4f68d727163e3cd6955f53340d75f
Kendall McGree <mcgreekendall@gmail.com>
2022-08-06 16:25:51 -0500

    refactor test for individual coin component

--- cypress/e2e/altCoin.cy.js
+++ cypress/e2e/altCoin.cy.js
@@ -1,30 +1,46 @@
 describe("Alt-Coin spec", () => {
   beforeEach(() => {
-    cy.fixture("alt-coin.json").then((results) => {
-      cy.intercept(
-        "GET",
-        `https://api.polygon.io/v2/aggs/ticker/X:ADAUSD/prev?adjusted=true&apiKey=mlvQmPrXbKKHEum7bADMetPy2uIJj4K4`,
-        results
-      );
-      cy.visit("http://localhost:3000");
-    });
-  });
-
-  it("should be able to choose Cardano and go to its landing page", () => {
+    cy.intercept(
+      "GET",
+      `https://api.polygon.io/v2/aggs/ticker/X:ADAUSD/prev?adjusted=true&apiKey=mlvQmPrXbKKHEum7bADMetPy2uIJj4K4`,
+      { fixture: "mockdata.json" }
+    );
+    cy.visit("http://localhost:3000");
     cy.get("select").select("CARDANO");
-    cy.get("button").eq(0).click();
+    cy.get(".search-coin").click();
   });
 
-  it.only("Should have all of Cardano's information populated after selection", () => {
+  it("Should show ADA's name Cardano", () => {
+    cy.get("h2").contains("CARDANO");
+  });
+
+  it("Should display Cardano's annual chart", () => {
+    cy.get(".chart").should("exist");
+    cy.get(".chart-display").should("be.visible");
+  });
+
+  it("Should have all of Cardano's information populated ", () => {
     cy.get(".chart").should("be.visible");
     cy.get("h2").contains("CARDANO");
-    cy.get("h3").contains("ADA");
-    cy.get(".previous-volume").should("exist");
+    cy.get("span").contains("ADA");
+    cy.get(".previous-volume").should("have.value", "");
     cy.get(".previous-high").should("exist");
-    cy.get(".previous-low").should("exist");
+    cy.get(".previous-low").should("have.value", "");
     cy.get(".previous-close").should("exist");
-    // cy.get(".coin-details").get(".annual-high-value").should("exist");
-    // cy.get(".annual-low-value").should("exist");
-    // cy.get(".annual-volume-value").should("exist");
+  });
+
+  it("Should have an about section at the bottom", () => {
+    cy.get("h2").contains("About");
+  });
+
+  it("Should have the ability to navigate back to the homepage and come back", () => {
+    cy.go("back")
+      .get(".coin-button")
+      .should("have.length.greaterThan", 2)
+      .go("forward");
+  });
+
+  it("Should be at the correct url", () => {
+    cy.url().should("include", "/coin/ADA");
   });
 });
